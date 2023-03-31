9b090c46adbcd017f3a4d83e2beec71d4099fbd7
cosmos hagan <cosmoshagan@cosmoss-MacBook-Pro.local>
2023-03-31 18:50:39 +0000

    Add logic folder and files

--- src/components/calculationButtons.js
+++ src/components/calculationButtons.js
@@ -1,29 +1,50 @@
-export default function CalculationButtons() {
+import { useState } from 'react';
+import calculate from '../logic/calculate';
+
+export default function CalcButtons() {
+  const [Object, setState] = useState({
+    total: '0',
+    next: null,
+    operation: null,
+  });
+
+  const calcAction = (buttonName) => {
+    const result = calculate(Object, buttonName);
+    setState({
+      next: result.next,
+      total: result.total,
+      operation: result.operation,
+    });
+  };
+
   return (
     <div className="calculator-grid">
       <div className="display">
-        <div className="previou-operand">  </div>
-        <div className="current-operand"> 0</div>
+        <div className="previou-operand">
+          {Object.total}
+          {' '}
+        </div>
+        <div className="current-operand">{Object.next}</div>
       </div>
-      <button type="button">AC</button>
-      <button type="button"> +/- </button>
-      <button type="button"> % </button>
-      <button type="button" className="operators"> + </button>
-      <button type="button"> 7 </button>
-      <button type="button"> 8 </button>
-      <button type="button"> 9 </button>
-      <button type="button" className="operators"> x </button>
-      <button type="button"> 4 </button>
-      <button type="button"> 5 </button>
-      <button type="button"> 6 </button>
-      <button type="button" className="operators"> - </button>
-      <button type="button"> 1 </button>
-      <button type="button"> 2 </button>
-      <button type="button"> 3 </button>
-      <button type="button" className="operators"> + </button>
-      <button type="button" className="span-two"> 0 </button>
-      <button type="button"> . </button>
-      <button type="button" className="operators"> = </button>
+      <button type="button" onClick={() => calcAction('AC')}>AC</button>
+      <button type="button" onClick={() => calcAction('+/-')}> +/- </button>
+      <button type="button" onClick={() => calcAction('%')}> % </button>
+      <button type="button" className="operators" onClick={() => calcAction('+')}> + </button>
+      <button type="button" onClick={() => calcAction('7')}> 7 </button>
+      <button type="button" onClick={() => calcAction('8')}> 8 </button>
+      <button type="button" onClick={() => calcAction('9')}> 9 </button>
+      <button type="button" className="operators" onClick={() => calcAction('x')}> x </button>
+      <button type="button" onClick={() => calcAction('4')}> 4 </button>
+      <button type="button" onClick={() => calcAction('5')}> 5 </button>
+      <button type="button" onClick={() => calcAction('6')}> 6 </button>
+      <button type="button" className="operators" onClick={() => calcAction('-')}> - </button>
+      <button type="button" onClick={() => calcAction('1')}> 1 </button>
+      <button type="button" onClick={() => calcAction('2')}> 2 </button>
+      <button type="button" onClick={() => calcAction('3')}> 3 </button>
+      <button type="button" className="operators" onClick={() => calcAction('+')}> + </button>
+      <button type="button" className="span-two" onClick={() => calcAction('0')}> 0 </button>
+      <button type="button" onClick={() => calcAction('.')}> . </button>
+      <button type="button" className="operators" onClick={() => calcAction('=')}> = </button>
     </div>
   );
 }
